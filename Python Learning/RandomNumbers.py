96ab598f9b0ee8375ffd7eb63a8c0566b12d449b
Reece Tunesi <rtunesi2000@gmail.com>
2024-11-12 10:25:54 +0000

    random Library learning

---
+++ Python Learning/RandomNumbers.py
@@ -0,0 +1,11 @@
+import random
+
+low = 1
+high = 100
+options = ("rock", "paper", "scissors")
+card = ["2", "3", "4", "5", "6", "7"]
+
+# number = random.randint(low, high)
+# number = random.random()
+# choice = random.choice(options)
+# random.shuffle(card)
