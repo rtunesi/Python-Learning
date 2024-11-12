591ab008aef9db7d81b103e1c4dffdee9af70ece
Reece Tunesi <rtunesi2000@gmail.com>
2024-11-12 10:32:56 +0000

    Exercise 10 Update

--- Python Learning/Exercises/Exercise10.py
+++ Python Learning/Exercises/Exercise10.py
@@ -4,6 +4,7 @@ import random
 
 computer_number = random.randint(1, 100)
 user_guess = 0
+guess_count = 0 
 
 print("Welcome to my python: random number guessing program.")
 
@@ -13,10 +14,11 @@ while True:
         user_guess = int(input(">: "))
         if user_guess > computer_number:
             print("The number is lower, try again.")
-            print("")
+            guess_count += 1
         else:
             print("The number is higher, try again.")
-            print("")
+            guess_count += 1
     else:
         print(f"Correct! The generated number was {computer_number}")
+        print(f"Your total number of guesses was: {guess_count}")
         break
