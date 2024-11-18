6212885db50362df624363f31efc58e1703d6966
Reece Tunesi <rtunesi2000@gmail.com>
2024-11-18 09:43:06 +0000

    Amended Folder Directory

---
+++ Python Learning/Lessons/MembershipOperators.py
@@ -0,0 +1,4 @@
+# membership operators = used to test whether a value or variable is found in a sequence
+#                        (string, list, tuple, set or dictionary)
+#                        1. in 2. not in
+
