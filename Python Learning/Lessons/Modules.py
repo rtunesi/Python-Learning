5b5235d15a5e8b6e9e187ca7fc62ef20936da9e3
Reece Tunesi <rtunesi2000@gmail.com>
2024-11-25 15:31:31 +0000

    Modules Learning

---
+++ Python Learning/Lessons/Modules.py
@@ -0,0 +1,13 @@
+# module = a file containing code you want to include in your program
+#          use 'import' to include a module (built-in or your own)
+#          useful to break up a large program reusable separate files
+
+import time
+import random
+import string
+import math as m
+
+print(m.pi)
+
+from math import pi
+print(pi)
